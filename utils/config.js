1bd02e91f4030a583325d043a26c5aaa79507d41
Ajithreddy2609 <155958613+Ajithreddy2609@users.noreply.github.com>
2025-11-08 12:40:18 +0530

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const config = {
   PORT: process.env.PORT || 8000,
-  COINGECKO_BASE_URL: process.env.COINGECKO_BASE_URL || 'https://api.coingecko.com/api/v3' ,
+  COINGECKO_BASE_URL: process.env.COINGECKO_BASE_URL || https://api.coingecko.com/api/v3,
   COINGECKO_API_KEY: process.env.COINGECKO_API_KEY || CG-S6ZKDfNSY8SQ9uUiN53sJyvG,
-  COINBASE_WS_URL: 'wss://ws-feed.exchange.coinbase.com',
+  COINBASE_WS_URL: wss://ws-feed.exchange.coinbase.com,
   CACHE_TTL: 120, // seconds
 };
 
